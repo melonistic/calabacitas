1972dabbe854fed4154e2737b1625bf1f9adb373
Santiago Alvarez-Vargas <santiago.v.alvarez@gmail.com>
2017-05-07 14:15:26 -0400

    added scenario

---
+++ features/step_definitions/landing_page.rb
@@ -0,0 +1,12 @@
+Given(/^I am a user with an open web browser$/) do
+  visit 'http://localhost:3000'
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I navigate to the Calabacitas page URL$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
