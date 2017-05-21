fcc67d29d3a2a4cf1727eb0b577babc30396b248
Bleachonce <heyyyinternet@gmail.com>
2017-05-21 14:21:35 -0400

    changing env file to set the browser for cucumber tests.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,22 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# features/support/env.rb
+
+# Clean the database before and after each scenario
+ Before do
+ DatabaseCleaner.start
+   end
+
+ After do |scenario|
+ DatabaseCleaner.clean
+ end
+
+# Register Chrome as the default driver
+  Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+
+  Capybara.javascript_driver = :chrome
+
