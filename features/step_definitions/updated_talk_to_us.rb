3bfb599fb9f6a166e76a7d2590281597b40dedce
Bleachonce <heyyyinternet@gmail.com>
2017-06-07 19:01:18 -0400

    made the test stuff

--- features/step_definitions/updated_talk_to_us.rb
+++ features/step_definitions/updated_talk_to_us.rb
@@ -7,11 +7,11 @@ When(/^I click on the "Talk to Us" link from the header navigation bar$/) do
 end
 
 Then(/^I see a form with 5 USER CUSTOMIZED text fields, an email address field, a free-text message field, and a "Send Love" button$/) do
-	find_field('talk_user_customized1').value
-	find_field('talk_user_customized2').value
-	find_field('talk_user_customized3').value
-	find_field('talk_user_customized4').value
-	find_field('talk_user_customized5').value
+	find_field('talk_dog').value
+	find_field('talk_cat').value
+	find_field('talk_monkey').value
+	find_field('talk_houseplant').value
+	find_field('talk_zebra').value
 	find_field('talk_email').value
 	find_field('talk_message').value
 	expect(find_button('Send Love'))
