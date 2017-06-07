35c268b49ffd297628ac7457c8f02071689d563e
Santiago Alvarez-Vargas <santiago.v.alvarez@gmail.com>
2017-06-06 21:30:29 -0400

    fixt test

--- features/step_definitions/updated_talk_to_us.rb
+++ features/step_definitions/updated_talk_to_us.rb
@@ -1,19 +1,19 @@
 Given(/^I am a user navigated to the home page$/) do
-	  visit 'http://localhost:3000'
+  visit 'http://localhost:3000'
 end
 
-When(/^I click "About your application's environment"$/) do
+When(/^I click on the "Talk to Us" link from the header navigation bar$/) do
 	  find("#talk-to-us").click
 end
 
 Then(/^I see a form with 5 USER CUSTOMIZED text fields, an email address field, a free-text message field, and a "Send Love" button$/) do
-	find_field('USER CUSTOMIZED1').value
-	find_field('USER CUSTOMIZED2').value
-	find_field('USER CUSTOMIZED3').value
-	find_field('USER CUSTOMIZED4').value
-	find_field('USER CUSTOMIZED5').value
-	find_field('Email').value
-	find_field('Message').value
-	expect(page). to have_css('#send-love', visible:true)
+	find_field('talk_user_customized1').value
+	find_field('talk_user_customized2').value
+	find_field('talk_user_customized3').value
+	find_field('talk_user_customized4').value
+	find_field('talk_user_customized5').value
+	find_field('talk_email').value
+	find_field('talk_message').value
+	expect(find_button('Send Love'))
 end
 
