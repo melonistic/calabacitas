d9446bcfadf2315013441fb68ba4fb6d293f3557
Santiago Alvarez-Vargas <santiago.v.alvarez@gmail.com>
2017-06-06 22:06:19 -0400

    fixt test

--- features/step_definitions/updated_landing_page_with_video.rb
+++ features/step_definitions/updated_landing_page_with_video.rb
@@ -2,11 +2,11 @@ When(/^I navigate to the Calabacitas home page$/) do
 	  visit 'http://localhost:3000'
 end
 
-Then(/^Then I see the home page and an embedded video$/) do
-	expect(page). to have_css('#video')
+Then(/^I see an embedded video on the page$/) do
+	expect(page.find('#video'))
 end
 
 And(/^I see a title for the video$/) do
-	  expect(page). to have_css('#heading-text', test:'USER CUSTOMIZED')
+	  expect(page.find('#home .heading-text')).to have_content 'USER CUSTOMIZED'
 end
 
